87b5329a0f1cb7563f6fe53b7104e510346b4888
Juliusz Szymajda <julekszymajda@gmail.com>
2025-05-29 16:46:32 +0200

    Internationalisation

--- src/main/java/WPFAT/configuration/SpringConfiguration.java
+++ src/main/java/WPFAT/configuration/SpringConfiguration.java
@@ -1,11 +1,19 @@
 package WPFAT.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -18,5 +26,30 @@ public class SpringConfiguration implements WebMvcConfigurer {
         viewResolver.setPrefix("/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Bean
+    MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/resources/i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("en"));
+        localeResolver.setCookieName("myLocaleCookie");
+        localeResolver.setCookieMaxAge(4800);
+        return localeResolver;
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
     }
 }
--- src/main/webapp/hello.jsp
+++ src/main/webapp/hello.jsp
@@ -1,9 +1,18 @@
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
 <html>
   <head>
-    <title>Hello there!</title>
+    <title><spring:message code="label.welcome"/></title>
   </head>
   <body>
-  <h3>Hello there!!!</h3>
+  <h3><spring:message code="label.welcome"/></h3>
   </body>
+
+  <div class="header">
+    <span style="float: right">
+    	<a style="background-color: white" href="?lang=pl">pl</a> | <a style="background-color: white" href="?lang=en">en</a>
+	</span>
+  </div>
+
 </html>
